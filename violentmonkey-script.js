44a4e77bf2d0076cc9dbb437061713caabc3b666
ZsoltSzunyoghy <zsolt.szunyoghy@gmail.com>
2020-04-20 21:29:50 +0200

    correct board size detection

--- violentmonkey-script.js
+++ violentmonkey-script.js
@@ -83,7 +83,7 @@ function createsgf(str){
   for(var i in lines){
     
     if (lines[i].includes("plays")){  
-      if (lines[i].includes("plays (O")){  
+      if (lines[i].includes("plays  (O")){  
         boardsize = 19;        
       }      
       output += "\nmove: " + lines[i];
